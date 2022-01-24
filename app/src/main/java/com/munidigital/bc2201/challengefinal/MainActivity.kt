3c6c2315d103c749e6fc0743ed71ca10c7654007
gaspar <gasparspalla@outlook.com>
2022-01-24 09:38:09 -0300

    segundo commit

--- app/src/main/java/com/munidigital/bc2201/challengefinal/MainActivity.kt
+++ app/src/main/java/com/munidigital/bc2201/challengefinal/MainActivity.kt
@@ -14,11 +14,17 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
 
+
+        super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        controlActionBar()
+
+    }
+
+    private fun controlActionBar(){
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
