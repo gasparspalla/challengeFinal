0745f45e085f4f3ab2ec0f781d575f7d76566629
gaspar <gasparspalla@outlook.com>
2022-01-24 09:12:17 -0300

    primer commit

---
+++ app/src/main/java/com/munidigital/bc2201/challengefinal/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.munidigital.bc2201.challengefinal.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/com/munidigital/bc2201/challengefinal/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.munidigital.bc2201.challengefinal.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
